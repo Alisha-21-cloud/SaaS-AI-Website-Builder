f7c25aceb0a8ce07c3a660bc29a3ccdaa96460e4
Alisha-21-cloud <ahmadalishasyed@gmail.com>
2025-07-27 18:24:08 +0530

    17 : billing

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -1,5 +1,6 @@
 import { inngest } from "@/inngest/client"
 import {prisma} from "@/lib/db"
+import { consumeCredits } from "@/lib/usage"
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init"
 
 import { TRPCError } from "@trpc/server"
@@ -49,6 +50,19 @@ export const messageRoute = createTRPCRouter({
 
             if (!existingProject) {
                 throw new TRPCError({ code: "NOT_FOUND", message: "Project not found" })
+            }
+
+            try {
+                await consumeCredits();
+            } catch (error) {
+                if (error instanceof Error) {
+                    throw new TRPCError({ code: "UNAUTHORIZED", message: error.message })
+                } else {
+                    throw new TRPCError({
+                        code: "TOO_MANY_REQUESTS",
+                        message: "You have reached your request limit"
+                    })
+                }
             }
             
             const createMessage = await prisma.message.create({
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,13 @@
 import { messageRoute } from '@/modules/messages/server/procedures';
 
 import { createTRPCRouter } from '../init';
+import { usageRouter } from '@/modules/usage/server/procedure';
 import { projectsRoute } from '@/modules/projects/server/procedures';
 
 export const appRouter = createTRPCRouter({
     message: messageRoute,
-    projects: projectsRoute
+    projects: projectsRoute,
+    usage: usageRouter
 })
 // export type definition of API
 export type AppRouter = typeof appRouter;
